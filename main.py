6f036a9cef62cde55f4624149097104a7326b66a
KETI_UnstructedWork <tjdgus6255@naver.com>
2023-03-15 13:33:49 +0900

    UDP_SERVER SONY_KINECT

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from threading import Thread
-from src.server import Server
+from src.server import Server, KinectServer
 
 LEFT_PORT = 5001
 RIGHT_PORT = 5002
+KINECT_PORT = 5008
 
 left = Server(LEFT_PORT)
 right = Server(RIGHT_PORT)
+kinect = KinectServer(KINECT_PORT)
 
 l_thr = Thread(target=left.run, args=())
 r_thr = Thread(target=right.run, args=())
+k_thr = Thread(target=kinect.run, args=())
 
 if __name__ == "__main__":
     l_thr.start()
     r_thr.start()
+    k_thr.start()
--- src/server.py
+++ src/server.py
@@ -3,8 +3,10 @@ import cv2
 import zlib
 import time
 import queue
+import pickle
 import socket
 import struct
+import numpy as np
 
 from nvjpeg import NvJpeg
 from datetime import datetime
@@ -31,7 +33,7 @@ class Server:
         self.tmp_data = b''
         self.all_data = None
 
-        self.img = None
+        self.rgb = None
 
         self.client_get_img_time = None
         self.server_get_img_time = None
@@ -138,7 +140,7 @@ class Server:
             if is_data_corrupted:
                 print("corrupted image has been deleted")
             else:
-                self.img = self.decomp.decode(self.all_data)
+                self.rgb = self.decomp.decode(self.all_data)
 
                 self.server_get_img_time = datetime.now().time().isoformat()
                 self.get_fps()
@@ -149,9 +151,57 @@ class Server:
 
                 self.show()
 
-                cv2.imshow(str(self.PORT), self.img)
+                cv2.imshow(str(self.PORT), self.rgb)
                 cv2.waitKey(1)
 
     def run(self):
         while True:
             self.recv_udp()
+
+class KinectServer(Server):
+    def __init__(self, PORT):
+        super().__init__(PORT)
+        self.imu   = None
+        self.depth = None
+
+        self.acc_xyz  = None
+        self.gyro_xyz = None
+
+    def recv_udp(self):
+        seg, addr = self.sock.recvfrom(self.MAX_DGRAM)
+        seg = seg.split(b'end')
+        if struct.unpack("B", seg[0])[0] > 1:
+            self.tmp_data += seg[7]
+        else:
+            self.client_get_img_time = seg[3].decode('utf-8')
+            self.tmp_data += seg[7]
+            self.all_data = self.tmp_data
+            self.tmp_data = b''
+
+            is_data_corrupted = self.checksum(seg[1])
+            if is_data_corrupted:
+                print("corrupted image has been deleted")
+            else:
+                self.all_data = self.all_data.split(b'frame')
+                self.imu = pickle.loads(seg[6])
+                self.acc_xyz = self.imu[0]
+                self.gyro_xyz = self.imu[1]
+                # print('Accelerometers:      ' + str(self.acc_xyz))
+                # print('Gyros:               ' + str(self.gyro_xyz))
+                self.rgb = self.decomp.decode(self.all_data[0])
+                self.depth = cv2.imdecode(np.asarray(bytearray(self.all_data[1]), dtype=np.uint8),
+                                          cv2.IMREAD_UNCHANGED)
+                self.server_get_img_time = datetime.now().time().isoformat()
+                self.get_fps()
+                self.get_mean_fps()
+
+                self.get_latency()
+                self.get_mean_latency()
+
+                self.show()
+
+                cv2.imshow(str(self.PORT) + '_color', self.rgb)
+                cv2.imshow(str(self.PORT) + '_depth', self.depth)
+
+                cv2.waitKey(1)
+
