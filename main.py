bb91091bddb57105374fc56c1ce257dcda95fad4
KETI_UnstructedWork <tjdgus6255@naver.com>
2023-05-12 15:16:49 +0900

    UDP SERVER MONO DEPTH

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-from threading import Thread
-from src.server import Server, KinectServer
+from src.server import Server
 
-LEFT_PORT = 5001
-RIGHT_PORT = 5002
-KINECT_PORT = 5008
+LEFT_PORT = 5053
 
 left = Server(LEFT_PORT)
-right = Server(RIGHT_PORT)
-kinect = KinectServer(KINECT_PORT)
-
-l_thr = Thread(target=left.run, args=())
-r_thr = Thread(target=right.run, args=())
-k_thr = Thread(target=kinect.run, args=())
 
 if __name__ == "__main__":
-    l_thr.start()
-    r_thr.start()
-    k_thr.start()
+    left.run()
